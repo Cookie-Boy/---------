8d4493e177481b440846789bb207365c73acd553
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2023-10-14 17:08:28 +0700

    Add description of black shawerma

--- JS/buttons.js
+++ JS/buttons.js
@@ -1,3 +1,7 @@
 document.getElementById('choice_btn').onclick = function(event) {
     window.open("https://www.google.com/maps/search/шаурмания", "_blank");
 }
+
+document.getElementById('order_black_btn').onclick = function(event) {
+    window.open("../order.html");
+}
