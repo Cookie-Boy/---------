caf4848c4b56ad737303db77d6ff461101dc700b
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2023-10-22 03:35:17 +0700

    Update basket design

--- JS/basket.js
+++ JS/basket.js
@@ -1,16 +1,66 @@
 let element;
-let purchaseList = document.getElementById("purchase-list");
-let main = document.getElementById("main");
-main.style.height = "124vh";
+const purchaseList = document.getElementById("purchase-list");
+const mainList = document.getElementById("main");
+const basketCounter = document.getElementById("basket-counter");
+const numberOfGoods = document.getElementById("number-goods");
+const resultCost = document.getElementById("result-sum");
+
+main.style.height = "100vh";
 let count = 0;
+let sum = 0;
 
-for (let i = 0; i < 10; i++) {
-    element = document.createElement("div");
-    element.classList.add("first");
+for (let j = 0; j < 2; j++) {
+for (let i = localStorage.length - 1; i >= 0; i--) {
+    let currentNode = JSON.parse(localStorage.getItem(localStorage.key(i)));
 
-    if (count > 4) {
-        main.style.height = String(30 * count) + "vh";
-    }
+    let element = document.createElement("div");
+    element.classList.add("shava" + String(i));
+
+    let image = document.createElement("img"); // фотка
+    image.classList.add("shava-image");
+    image.src = currentNode.imageSrc;
+    element.append(image);
+
+    let textBlock = document.createElement("div"); // текстовый блок с названием и описанием
+    let header = document.createElement("h5");
+    header.textContent = currentNode.name;
+    let composition = document.createElement("p");
+    composition.textContent = "Состав: " + currentNode.composition;
+    let weight = document.createElement("p");
+    weight.textContent = "Вес: " + currentNode.weight + " гр.";
+    textBlock.classList.add("text-block");
+    textBlock.append(header, composition, weight);
+    element.append(textBlock);
+
+    let counter = document.createElement("div"); // плюс минус
+    counter.classList.add("element-counter");
+    let counterMinus = document.createElement("button");
+    counterMinus.classList.add("counter-minus");
+    counterMinus.textContent = "-";
+    let counterText = document.createElement("h5");
+    counterText.classList.add("counter-text");
+    counterText.textContent = "1";
+    let counterPlus = document.createElement("button");
+    counterPlus.classList.add("counter-plus");
+    counterPlus.textContent = "+";
+    counter.append(counterMinus, counterText, counterPlus);
+    element.append(counter);
+
+    let price = document.createElement("div");
+    price.classList.add("price-block");
+    price.textContent = currentNode.cost + " р.";
+    element.append(price);
+
     purchaseList.appendChild(element);
     count++;
+    sum += currentNode.cost;
 }
+}
+
+if (count > 3) {
+    mainList.style.height = String(30 * count) + "vh";
+}
+
+basketCounter.textContent = "Корзина: " + String(count);
+numberOfGoods.textContent = String(count) + " шт.";
+resultCost.textContent = String(sum) + " р.";
--- JS/localStorage.js
+++ JS/localStorage.js
@@ -1,5 +1,6 @@
-class shaverma {
+class Shaverma {
 
+    name;
     imageSrc;
     description;
     weight;
@@ -7,7 +8,8 @@ class shaverma {
     composition;
     isSelected = false;
 
-    constructor(imageSrc, description, weight, cost, composition) {
+    constructor(name, imageSrc, description, weight, cost, composition) {
+        this.name = name;
         this.imageSrc = imageSrc;
         this.description = description;
         this.weight = weight;
@@ -16,8 +18,8 @@ class shaverma {
     }
 }
 
-black = new shaverma('images/header_page/black_shaverma.jpeg', 'Вкусная чёрная шава, хороша для негров', 340, 400, 'Черный лаваш, свинина, картошка фри, маринованный огурчик, свежий помидор, чёрный фирменный соус, краситель: натуральные чернила осьминога');
-gigant = new shaverma('images/header_page/XXL_shaverma.png', 'Большая шаурма для больших мальчиков', 600, 600, 'лаваш, говядина, куриное филе, картошка фри, перец халапеньо, свежий помидор, маринованный огурчик, корейская морковка, острый соус');
+black = new Shaverma('Шаурма "Black"', 'images/header_page/black_shaverma.jpeg', 'Вкусная чёрная шава, хороша для негров', 340, 400, 'Черный лаваш, свинина, картошка фри, маринованный огурчик, свежий помидор, чёрный фирменный соус, краситель: натуральные чернила осьминога');
+gigant = new Shaverma('Шаурма Гигант', 'images/header_page/XXL_shaverma.png', 'Большая шаурма для больших мальчиков', 600, 600, 'Лаваш, говядина, куриное филе, картошка фри, перец халапеньо, свежий помидор, маринованный огурчик, корейская морковка, острый соус');
 
 localStorage.setItem('black', JSON.stringify(black));
-localStorage.setItem('gigant', JSON.stringify(gigant));s
+localStorage.setItem('gigant', JSON.stringify(gigant));
