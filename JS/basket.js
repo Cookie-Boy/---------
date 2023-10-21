803562e1319026b821934dd61dd5d5b201d213af
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2023-10-21 18:56:41 +0700

    Update to basket

--- JS/basket.js
+++ JS/basket.js
@@ -1,6 +1,16 @@
-let element = document.createElement("div");
-element.classList.add("first");
-
+let element;
 let purchaseList = document.getElementById("purchase-list");
+let main = document.getElementById("main");
+main.style.height = "124vh";
+let count = 0;
 
-purchaseList.appendChild(element);
+for (let i = 0; i < 10; i++) {
+    element = document.createElement("div");
+    element.classList.add("first");
+
+    if (count > 4) {
+        main.style.height = String(30 * count) + "vh";
+    }
+    purchaseList.appendChild(element);
+    count++;
+}
