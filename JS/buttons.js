1b60d4f10b0827af128b8a0bdf88dc93b1671f24
Ghenya <ghenyadog@bk.ru>
2023-10-21 16:25:15 +0700

    Убрал кнопку заказать и добавио пару шав в localstorage

--- JS/buttons.js
+++ JS/buttons.js
@@ -1,7 +1,6 @@
 document.getElementById('choice_btn').onclick = function(event) {
     window.open("https://www.google.com/maps/search/шаурмания", "_blank");
 }
-
 document.getElementById('vk').onclick = function(event) {
     
     window.open("https://vk.com/im_banana_tyryryry", "_blank");
@@ -11,14 +10,12 @@ document.getElementById('tg').onclick = function(event) {
     window.open("https://t.me/+BaDiNvvEAOIyOGFi", "_blank");
 }
 
+
 document.getElementById('mail').onclick = function(event) {
     window.open("https://ghenyadog@bk.ru", "_blank");
-}
-
-document.getElementById('order_black_btn').onclick = function(event) {
-    window.open("order.html", "_blank");
 }
 
 document.getElementById('git').onclick = function(event) {
     window.open("https://github.com/Cookie-Boy/Shaverma", "_blank");
 }
+
