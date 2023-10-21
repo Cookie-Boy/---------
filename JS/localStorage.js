0d1e661b11f65b875abac959922e75e0c53ec564
Ghenya <ghenyadog@bk.ru>
2023-10-21 18:56:21 +0700

    Сделал меню и сборку блоков товаров в цикле, а так же их вывод на странице меню

--- JS/localStorage.js
+++ JS/localStorage.js
@@ -16,13 +16,11 @@ class shaverma {
     }
 }
 
-black = new shaverma('images/header_page/black_shaverma.jpeg', 'Вкусная чёрная шава, хороша для негров', 340, 400, 'Черный лаваш, свинина, картошка фри, маринованный огурчик, свежий помидор, чёрный фирменный соус, краситель: натуральные чернила осминога');
+black = new shaverma('images/header_page/black_shaverma.jpeg', 'Вкусная чёрная шава, хороша для негров', 340, 400, 'Черный лаваш, свинина, картошка фри, маринованный огурчик, свежий помидор, чёрный фирменный соус, краситель: натуральные чернила осьминога');
+gigant = new shaverma('images/header_page/XXL_shaverma.png', 'Большая шаурма для больших мальчиков', 600, 600, 'лаваш, говядина, куриное филе, картошка фри, перец халапеньо, свежий помидор, маринованный огурчик, корейская морковка, острый соус');
 
-gigant = new shaverma('images/header_page/XXL_shaverma.png', 'Большая шаурма для больших мальчиков', 600, 600, 'лаваш, говядина, куриное филе, картошка фри, перец халапеньо, свежий помидор, маринованный огурчик, корейская морковка, острый соус')
-
-localStorage.setItem('Black', JSON.stringify(black));
+localStorage.setItem('black', JSON.stringify(black));
 localStorage.setItem('gigant', JSON.stringify(gigant));
-
 
 
 
---
+++ JS/menu.js
@@ -0,0 +1,70 @@
+for(let i = localStorage.length - 1; i >= 0; i--)
+{
+
+let element = document.createElement('div');
+element.classList.add('shavaObj'+ String(i));
+
+const img = document.createElement('img');
+img.src = JSON.parse(localStorage.getItem(localStorage.key(i))).imageSrc;
+element.append(img);
+
+const text = document.createTextNode(JSON.parse(localStorage.getItem(localStorage.key(i))).composition);
+element.append(text)
+
+let shavaList = document.getElementById('shavaList');
+shavaList.appendChild(element);
+
+}
+
+for(let i = localStorage.length - 1; i >= 0; i--)
+{
+
+let element = document.createElement('div');
+element.classList.add('shavaObj'+ String(i));
+
+const img = document.createElement('img');
+img.src = JSON.parse(localStorage.getItem(localStorage.key(i))).imageSrc;
+element.append(img);
+
+const text = document.createTextNode(JSON.parse(localStorage.getItem(localStorage.key(i))).composition);
+element.append(text)
+
+let shavaList = document.getElementById('shavaList');
+shavaList.appendChild(element);
+
+}
+for(let i = localStorage.length - 1; i >= 0; i--)
+{
+
+let element = document.createElement('div');
+element.classList.add('shavaObj'+ String(i));
+
+const img = document.createElement('img');
+img.src = JSON.parse(localStorage.getItem(localStorage.key(i))).imageSrc;
+element.append(img);
+
+const text = document.createTextNode(JSON.parse(localStorage.getItem(localStorage.key(i))).composition);
+element.append(text)
+
+let shavaList = document.getElementById('shavaList');
+shavaList.appendChild(element);
+
+}
+
+for(let i = localStorage.length - 1; i >= 0; i--)
+{
+
+let element = document.createElement('div');
+element.classList.add('shavaObj'+ String(i));
+
+const img = document.createElement('img');
+img.src = JSON.parse(localStorage.getItem(localStorage.key(i))).imageSrc;
+element.append(img);
+
+const text = document.createTextNode(JSON.parse(localStorage.getItem(localStorage.key(i))).composition);
+element.append(text)
+
+let shavaList = document.getElementById('shavaList');
+shavaList.appendChild(element);
+
+}
