6f9f10b81f4c03414098d48da6b6536ec7128a6d
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2023-10-31 01:46:16 +0700

    Update basket functionality

--- JS/basket.js
+++ JS/basket.js
@@ -1,121 +1,145 @@
-class Order {
-    name;
-    weight;
-    quantity;
-    cost;
-
-    constructor(name, weight, quantity, cost) {
-        this.name = name;
-        this.weight = weight;
-        this.quantity = quantity;
-        this.cost = cost;
-    }
-}
-
-let count = 0, sum = 0;
-const ordersArray = [];
+let count = 0;
 const purchaseList = document.getElementById("purchase-list");
 const mainList = document.getElementById("main");
 const basketCounter = document.getElementById("basket-counter");
 const numberOfGoods = document.getElementById("number-goods");
 const resultCost = document.getElementById("result-sum");
 
-for (let j = 0; j < 4; j++) {
-for (let i = localStorage.length - 1; i >= 0; i--) {
+function getResultSum() {
+    let sum = 0;
+    for (let i = 0; i < localStorage.length; i++) {
+        let currentNode = JSON.parse(localStorage.getItem(localStorage.key(i)));
+
+        if (currentNode.isSelected == true)
+            sum += currentNode.cost * currentNode.quantity;
+    }
+    return sum;
+}
+
+function getQuantity() {
+    let quantity = 0;
+    for (let i = 0; i < localStorage.length; i++) {
+        let currentNode = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        
+        if (currentNode.isSelected == true)
+            quantity += currentNode.quantity;
+    }
+    return quantity;
+}
+
+function setAllQuantitative() {
+    basketCounter.textContent = "Корзина: " + String(getQuantity());
+    numberOfGoods.textContent = String(getQuantity()) + " шт.";
+    resultCost.textContent = String(getResultSum()) + " р.";
+}
+
+for (let i = 0; i < localStorage.length; i++) {
     let currentNode = JSON.parse(localStorage.getItem(localStorage.key(i)));
 
-    let element = document.createElement("div");
-    element.classList.add("item");
-    element.classList.add("shava" + String(i));
+    if (currentNode.isSelected == true) {
+        let element = document.createElement("div");
+        element.classList.add("item");
+        element.id = "basket-" + i;
 
-    let image = document.createElement("img"); // Фотка
-    image.classList.add("shava-image");
-    image.src = currentNode.imageSrc;
-    element.append(image);
+        let info = document.createElement("div");
+        info.classList.add("info");
+    
+        let image = document.createElement("img"); // Фотка
+        image.classList.add("shava-image");
+        image.src = currentNode.imageSrc;
+        info.append(image);
+    
+        let textBlock = document.createElement("div"); // Текстовый блок с названием и описанием
+        let header = document.createElement("h5");
+        header.textContent = currentNode.name;
+        let composition = document.createElement("p");
+        composition.textContent = "Состав: " + currentNode.composition;
+        let weight = document.createElement("p");
+        weight.textContent = "Вес: " + currentNode.weight + " гр.";
+        textBlock.classList.add("text-block");
+        textBlock.append(header, composition, weight);
+        info.append(textBlock);
+    
+        let counter = document.createElement("div"); // Плюс минус
+        counter.classList.add("element-counter");
+        let counterMinus = document.createElement("button");
+        counterMinus.classList.add("counter-minus");
+        counterMinus.textContent = "–";
+        if (currentNode.quantity > 1)
+            counterMinus.classList.add("counter-minus-active");
 
-    let textBlock = document.createElement("div"); // Текстовый блок с названием и описанием
-    let header = document.createElement("h5");
-    header.textContent = currentNode.name;
-    let composition = document.createElement("p");
-    composition.textContent = "Состав: " + currentNode.composition;
-    let weight = document.createElement("p");
-    weight.textContent = "Вес: " + currentNode.weight + " гр.";
-    textBlock.classList.add("text-block");
-    textBlock.append(header, composition, weight);
-    element.append(textBlock);
+        let counterText = document.createElement("h5");
+        counterText.classList.add("counter-text");
+        counterText.textContent = currentNode.quantity;
 
-    let counter = document.createElement("div"); // Плюс минус
-    counter.classList.add("element-counter");
-    let counterMinus = document.createElement("button");
-    counterMinus.classList.add("counter-minus");
-    counterMinus.textContent = "–";
-    let counterText = document.createElement("h5");
-    counterText.classList.add("counter-text");
-    counterText.textContent = "1";
-    let counterPlus = document.createElement("button");
-    counterPlus.classList.add("counter-plus");
-    counterPlus.classList.add("counter-plus-active");
-    counterPlus.textContent = "+";
-    counter.append(counterMinus, counterText, counterPlus);
-    element.append(counter);
+        let counterPlus = document.createElement("button");
+        counterPlus.classList.add("counter-plus");
+        counterPlus.classList.add("counter-plus-active");
+        counterPlus.textContent = "+";
+        counter.append(counterMinus, counterText, counterPlus);
+        info.append(counter);
+    
+        let price = document.createElement("div"); // Цена
+        price.classList.add("price-block");
+        price.textContent = currentNode.cost * currentNode.quantity + " р.";
+        info.append(price);
 
-    let price = document.createElement("div"); // Цена
-    price.classList.add("price-block");
-    price.textContent = currentNode.cost + " р.";
-    element.append(price);
+        let trashButton = document.createElement("button");
+        trashButton.classList.add("trash-btn");
 
-    purchaseList.appendChild(element);
-    ordersArray.push(new Order(currentNode.name, currentNode.weight, 1, currentNode.cost));
-    count++;
-    sum += currentNode.cost;
-}
+        let trashImage = document.createElement("img"); // Фотка
+        trashImage.classList.add("trash-image");
+        trashImage.src = "images/icons/trash.png";
+        trashImage.alt = "Удалить";
+        trashButton.append(trashImage);
+
+        element.append(info, trashButton);
+    
+        purchaseList.appendChild(element);
+        count++;
+
+        // Обработка нажатий на плюс и минус
+
+        counterPlus.onclick = function(event) {
+            if (currentNode.quantity == 1) {
+                counterMinus.classList.add("counter-minus-active");
+            }
+            currentNode.quantity++;
+            localStorage.setItem(i, JSON.stringify(currentNode));
+            counterText.textContent = currentNode.quantity;
+            price.textContent = String(currentNode.cost * currentNode.quantity) + " р.";
+            setAllQuantitative();
+        }
+
+        counterMinus.onclick = function(event) {
+            if (currentNode.quantity == 2) {
+                counterMinus.classList.remove("counter-minus-active");
+            }
+            
+            if (currentNode.quantity >= 2) {
+                currentNode.quantity--;
+                localStorage.setItem(i, JSON.stringify(currentNode));
+                counterText.textContent = currentNode.quantity;
+            }
+    
+            price.textContent = String(currentNode.cost * currentNode.quantity) + " р.";
+            setAllQuantitative();
+        }
+
+        // Удаление элемента из корзины
+
+        trashButton.addEventListener("click", () => {
+            currentNode.isSelected = false;
+            currentNode.quantity = 1;
+            localStorage.setItem(i, JSON.stringify(currentNode));
+            purchaseList.removeChild(element);
+            setAllQuantitative();
+        })
+    }
 }
 
 if (count > 3) {
     mainList.style.height = String(30 * count) + "vh";
 }
 
-basketCounter.textContent = "Корзина: " + String(count);
-numberOfGoods.textContent = String(count) + " шт.";
-resultCost.textContent = String(sum) + " р.";
-
-// Обработка нажатий на плюс и минус
-
-Array.from(document.getElementsByClassName("item")).forEach(
-    function(element, i) {
-        element.querySelector(".counter-plus").onclick = function(event) {
-
-            if (ordersArray[i].quantity == 1) {
-                element.querySelector(".counter-minus").classList.add("counter-minus-active");
-            }
-
-            ordersArray[i].quantity++;
-            count++;
-            sum += ordersArray[i].cost;
-            element.querySelector(".counter-text").textContent = ordersArray[i].quantity;
-            element.querySelector(".price-block").textContent = String(ordersArray[i].cost * ordersArray[i].quantity) + " р.";
-            basketCounter.textContent = "Корзина: " + String(count);
-            numberOfGoods.textContent = String(count) + " шт.";
-            resultCost.textContent = String(sum) + " р.";
-        }
-
-        element.querySelector(".counter-minus").onclick = function(event) {
-            
-            if (ordersArray[i].quantity == 2) {
-                element.querySelector(".counter-minus").classList.remove("counter-minus-active");
-            }
-            
-            if (ordersArray[i].quantity >= 2) {
-                ordersArray[i].quantity--;
-                count--;
-                sum -= ordersArray[i].cost;
-                element.querySelector(".counter-text").textContent = ordersArray[i].quantity;
-            }
-
-            element.querySelector(".price-block").textContent = String(ordersArray[i].cost * ordersArray[i].quantity) + " р.";
-            basketCounter.textContent = "Корзина: " + String(count);
-            numberOfGoods.textContent = String(count) + " шт.";
-            resultCost.textContent = String(sum) + " р.";
-        }
-    }
-);
+setAllQuantitative();
--- JS/menu.js
+++ JS/menu.js
@@ -1,20 +1,38 @@
-for(let j = 0; j < 8; j++)
+for(let i = 0; i < localStorage.length; i++)
 {
-for(let i = localStorage.length - 1; i >= 0; i--)
-{
+    let currentNode = JSON.parse(localStorage.getItem(localStorage.key(i)));
 
-let element = document.createElement('div');
-element.classList.add('shavaObj'+ String(i));
+    let element = document.createElement('div');
+    element.classList.add('shavaObj'+ String(i));
 
-const img = document.createElement('img');
-img.src = JSON.parse(localStorage.getItem(localStorage.key(i))).imageSrc;
-element.append(img);
+    const img = document.createElement('img');
+    img.src = currentNode.imageSrc;
+    element.append(img);
 
-const text = document.createTextNode(JSON.parse(localStorage.getItem(localStorage.key(i))).composition);
-element.append(text)
+    const text = document.createTextNode(currentNode.composition);
+    element.append(text)
+    const button = document.createElement("button");
+    button.style.color = "black";
 
-let shavaList = document.getElementById('shavaList');
-shavaList.appendChild(element);
+    if (currentNode.isSelected == false) {
+        button.textContent = "Добавить в корзину";
+    } else {
+        button.textContent = "В корзине";
+    }
 
-}
+    element.append(button);
+
+    button.addEventListener("click", () => {
+        if (currentNode.isSelected == false) {
+            currentNode.isSelected = true;
+            button.textContent = "В корзине";
+        } else {
+            currentNode.isSelected = false;
+            button.textContent = "Добавить в корзину";
+        }
+        localStorage.setItem(i, JSON.stringify(currentNode));
+    });
+
+    let shavaList = document.getElementById('shavaList');
+    shavaList.appendChild(element);
 }
