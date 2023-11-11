6437861738ff748450b66bb39c6efb1a43839852
Ghenya <ghenyadog@bk.ru>
2023-11-11 17:15:13 +0700

    Добавил бургер меню

---
+++ JS/burgerMenu.js
@@ -0,0 +1,3 @@
+let burgerMenu = document.querySelector('.burgerMenu');
+
+burgerMenu.addEventListener('click', function(){document.querySelector('.menu').classList.toggle('active'); document.querySelector('.temno').classList.toggle("active")})
