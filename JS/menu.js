560d05b2b560bc565f7f9324dcbd8b98d2564d0d
Ghenya <ghenyadog@bk.ru>
2023-11-18 02:20:01 +0700

    Доделал стиль для меню

--- JS/menu.js
+++ JS/menu.js
@@ -5,16 +5,29 @@ for (let i = 0; i < 2; i++)
 for (let id in goods) {
     let element = document.createElement('div');
     element.classList.add('shavaObj');
+    element.id = id;
 
     const img = document.createElement('img');
     img.src = goods[id].imageSrc;
-    img.classList.add("photo");
+    img.addEventListener("click", function() {
+                                                
+                                                    })
     element.append(img);
 
-    const text = document.createTextNode(goods[id].composition);
-    element.append(text)
+    const weight = document.createElement('div');
+    const text = document.createTextNode(goods[id].weight + " г");
+    weight.classList.add('weight');
+    weight.append(text);
+    element.append(weight);
+
+    const cost = document.createElement('div');
+    cost.classList.add('cost');
+    const text1 = document.createTextNode(goods[id].cost + " ₽");
+    cost.append(text1);
+    element.append(cost);
+
     const button = document.createElement("button");
-    button.style.color = "black";
+    button.classList.add('addButton');
 
     if (basket[id]) {
         button.textContent = "В корзине";
